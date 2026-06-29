796668d93441e1dcad6f4428622f13bd4f9dd732
henriquejhc81-sudo <henriquejhc81@gmail.com>
2026-06-29 00:12:51 -0300

    Update gamestate.py

--- gamestate.py
+++ gamestate.py
@@ -1,13 +1,11 @@
 from typing import TypeVar
 import dataclasses
-
 import streamlit as st
 
 StateT = TypeVar('StateT')
 
 def persistent_game_state(initial_state: StateT) -> StateT:
-    session_id = st.report_thread.get_report_ctx().session_id
-    session = st.server.server.Server.get_current()._get_session_info(session_id).session
-    if not hasattr(session, '_gamestate'):
-        setattr(session, '_gamestate', initial_state)
-    return session._gamestate
+    # Utiliza o st.session_state moderno para manter o estado do jogo salvo
+    if '_gamestate' not in st.session_state:
+        st.session_state['_gamestate'] = initial_state
+    return st.session_state['_gamestate']
